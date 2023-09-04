67e6a304510852e60f65c53cf2d17a776057a666
gabrielwerta <133909936+gabrielwerta@users.noreply.github.com>
2023-09-04 15:08:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//constantes
 const botaoDesafio = document.getElementById("botao-desafio");
 const telaPropostaDesafio = document.getElementById("proposta-desafio");
 const telaDesafio = document.getElementById("desafio");
@@ -5,8 +6,11 @@ const respostaDesafio = document.getElementById("resposta-desafio");
 const botaoEnviarResposta = document.getElementById("botao-enviar-resposta");
 const telaConteudoRevelado = document.getElementById("conteudo-revelado");
 
+//código para esconder telas que serão mostradas após o desafio
 telaDesafio.style.display = "none";
 telaConteudoRevelado.style.display = "none";
+
+//função que faz mostrar o desafio ao apertar no botão
 botaoDesafio.addEventListener("click", () => {
     telaDesafio.style.display = "block";
     botaoDesafio.style.display = "none";
